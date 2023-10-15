bea3e635f4fd9e91734a0808da02ab877ef72539
Sourav <sourav@Souravs-MacBook-Air.local>
2023-10-16 01:37:20 +0600

    mongodb faltu

--- Programming with Mosh/issue-tracker/app/api/issues/route.ts
+++ Programming with Mosh/issue-tracker/app/api/issues/route.ts
@@ -1,6 +1,6 @@
+import prisma from "@/prisma/client"
 import { NextResponse } from "next/server"
 import z from "zod"
-import prisma from "@/prisma/client"
 
 const createIssueSchema = z.object({
    title: z.string().min(1),
@@ -9,19 +9,21 @@ const createIssueSchema = z.object({
 
 export async function POST(request: Request) {
    const body = await request.json()
-   const validation = createIssueSchema.safeParse(body)
 
+   const validation = createIssueSchema.safeParse(body)
    if (!validation.success) {
       return NextResponse.json(validation.error.errors, { status: 400 })
    }
    const { title, description } = validation.data
 
-   const newIssue = await prisma.issue.create({
-      data: {
-         title: title,
-         description: description,
-      },
-   })
-
-   return NextResponse.json(newIssue, { status: 201 })
+   try {
+      const newIssue = await prisma.issue.create({
+         data: { title: title, description: description },
+      })
+      return NextResponse.json(newIssue, { status: 201 })
+   } catch (error) {
+      return NextResponse.json("The error is - " + error, { status: 500 })
+   } finally {
+      await prisma.$disconnect()
+   }
 }
